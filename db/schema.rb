c368fe57b50bbf74dd5d890ef5f68f359fda3f1f
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-15 22:35:56 -0500

    add the relation between user and products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216022902) do
+ActiveRecord::Schema.define(version: 20161216031654) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 20161216022902) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

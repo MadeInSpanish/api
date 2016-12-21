129b18d29cc63e829f35db4cc2ebda3c62667606
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-21 13:04:39 -0500

    remove slug  from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221171701) do
+ActiveRecord::Schema.define(version: 20161221180129) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20161221171701) do
     t.string   "info"
     t.string   "name"
     t.string   "phone_number"
-    t.string   "slug"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "type"

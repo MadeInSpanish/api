514fde30c4d7abc0927903ac7e0fdbb3ed4bda94
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-25 13:54:51 -0500

    override the json_key_format

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -1,7 +1,8 @@
 JSONAPI.configure do |config|
   # built in paginators are :none, :offset, :paged
   config.default_paginator = :paged
-
   config.default_page_size = 10
   config.maximum_page_size = 20
+  config.json_key_format = :camelized_key
+  config.use_text_errors = true
 end

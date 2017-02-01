fd510b5af19becbd9e870ac5808789c26ce9ccb4
Ana Maria Sosa <anamariasosam@gmail.com>
2017-02-01 16:12:14 -0500

    add the posibility to search the stores with a spefic tag

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,6 @@ class Store < User
   has_many :products
 
   acts_as_taggable_on :tags
+  
+  scope :by_join_date, ->{ order('created_at DESC') }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   use_doorkeeper
+
   namespace :v1 do
     jsonapi_resources :products
     jsonapi_resources :categories
     jsonapi_resources :users
     jsonapi_resources :stores
     jsonapi_resources :customers
+    get 'tags/:tag', to: 'tags#show', as: :tag
     jsonapi_resources :tags
   end
 end

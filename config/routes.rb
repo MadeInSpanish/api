2e9487faa375a6fe7a57cebbfdff02841f8e3dc6
Ana Maria Sosa <anamariasosam@gmail.com>
2017-04-10 21:58:14 -0500

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Api definition
+  namespace :v1, defaults: { format: :json } do
+      resources :sessions, :only => [:create, :destroy]
+      resources :users
+  end
 end
